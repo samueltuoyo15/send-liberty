33bff48a24e2998fe32fb8ebb8e9e6e356c03c5b
samueltuoyo15 <samueltuoyo9082@gmail.com>
2026-02-19 11:08:47 +0100

    update validation

--- server/src/middlewares/validate.resource.ts
+++ server/src/middlewares/validate.resource.ts
@@ -1,34 +1,22 @@
-import { z } from "zod";
 import { Request, Response, NextFunction } from "express";
-import { ZodError } from "zod";
-
-export type RequestSchema = z.ZodObject<{
-    body: z.ZodType;
-    query?: z.ZodType;
-    params?: z.ZodType;
-}>;
-
+import { ZodType, ZodError } from "zod";
 
 export const validateResource =
-    <T extends RequestSchema>(schema: T) =>
+    (schema: ZodType) =>
         async (req: Request, res: Response, next: NextFunction) => {
             try {
-                const parsed = await schema.parseAsync({
+                await schema.parseAsync({
                     body: req.body,
                     query: req.query,
                     params: req.params,
                 });
-
-                req.body = parsed.body;
-                req.query = parsed.query;
-                req.params = parsed.params;
 
                 return next();
             } catch (error) {
                 if (error instanceof ZodError) {
                     return res.status(400).json({
                         success: false,
-                        message: error.issues.map((e) => e.message).join(", "),
+                        message: error.issues.map(e => e.message).join(", "),
                     });
                 }
                 return next(error);
